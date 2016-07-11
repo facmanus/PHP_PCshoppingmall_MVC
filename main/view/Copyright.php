e247408bef289c4f5e3d41a4a18aa1f0cf422cdf
キムヨンボム(용가리/superblr) <manniere@naver.com>
2016-07-12 03:22:16 +0900

    Update Copyright.php

--- main/view/Copyright.php
+++ main/view/Copyright.php
@@ -1,4 +1,4 @@
 <p align="center">PHP기반 ShopingMall, Develop Use Skill : <kbd>HTML / PHP / CSS / Bootstrap / JavaScript / AngularJS</kbd></p>
 
-<p align="center">Copyright ©<kbd>영진전문대 컴퓨터 정보계열 WDj-2 1201061김용범</kbd> Co., Ltd. All Rights Reserved.</p>
+<p align="center">Copyright ©<kbd>영진#전문#대 컴퓨터 #정보계열 WD#j-2 1201#061김#용#범</kbd> Co., Ltd. All Rights Reserved.</p>
 
